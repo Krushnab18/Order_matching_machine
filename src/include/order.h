984821d0f17b997bf858ad38acc1cba7744fb9e6
Krushna <krushnabankar79@gmail.com>
2024-10-21 11:50:44 +0530

    changes some files

--- src/include/order.h
+++ src/include/order.h
@@ -1,3 +1,19 @@
-typedef struct order {
+typedef struct Time{
+    short hours;
+    short minute;
+    short second;
+}Time;
 
-}
+typedef struct Order{
+    long long order_id;
+    int quantity;
+    double price;
+    Time time_stamp;
+    char stock_symbol[15];
+    char side;
+}Order;
+
+void create_new_order(long long order_id, int quantity, double price, Time time_stamp, char *stock_symbol, char side);
+void print_order(const Order order);
+void free_order(Order *order);
+int compareOrders(const Order* order1, const Order* order2);
